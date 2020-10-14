89c875ceb97f4eb4f76106b11020eb388d4bcd17
shruti1025 <shrutiaggrwal95@gmail.com>
2020-10-14 15:40:32 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,30 @@
+class Plinko {
+    constructor(x,y){
+        var options={
+        friction:0,
+            restitution:1,
+            isStatic:true
+        
+        }
+        this.radius=10;
+        this.body=Bodies.circle(x,y,this.radius,options);
+        
+        World.add(world,this.body);
+        }
+        
+        display(){
+            var pos=this.body.position;
+            var angle = this.body.angle;
+
+            push();
+            translate(pos.x, pos.y);
+            rotate(angle);
+            imageMode(CENTER);
+            noStroke();
+            fill("white");
+            ellipseMode(RADIUS);
+            ellipse(0,0,this.radius,this.radius);
+            pop();
+        }
+        
+}
